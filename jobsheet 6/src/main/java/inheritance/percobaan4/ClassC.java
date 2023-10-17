6a139d9339d732b7d25f273ffd3f1808b4f3392a
FarrelEdric <144577200+FarrelEdric@users.noreply.github.com>
2023-10-17 21:27:35 +0700

    Add files via upload

---
+++ jobsheet 6/src/main/java/inheritance/percobaan4/ClassC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package inheritance.percobaan4;
+
+/**
+ *
+ * @author Farrel Edric
+ */
+public class ClassC extends ClassB {
+    ClassC() {
+        super(); // Panggil konstruktor superclass (ClassB)
+        System.out.println("konstruktor C dijalankan");
+    }
+}
